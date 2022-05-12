f02991ba71d9cc994d81889eea44ae219f71c036
Tyler French <tylerfrench89@gmail.com>
2022-05-11 18:43:41 -0700

    finished repeat string excersise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(word, times) {
+    let str = ""
+    if(times < 0) {
+        return 'ERROR'
+    }
+    for(let i = times; i > 0; i--) {
+        str += word
+    }
+    return str
 };
 
 // Do not edit below this line
