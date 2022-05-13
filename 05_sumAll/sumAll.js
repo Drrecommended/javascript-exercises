13d3f67794058aecb93e2dfa16cc3019f0d93bbf
Tyler French <tylerfrench89@gmail.com>
2022-05-12 20:23:01 -0700

    test

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,25 @@
-const sumAll = function() {
+const sumAll = function (...args) {
+  let fullArr = []
+  let sum = 0
+  const reducer = (accumulator, currentValue) => accumulator + currentValue
+  args.sort((a, b) => {
+    return a - b
+  })
+  if (
+    args[0] < 0 ||
+    typeof args[0] !== 'number' ||
+    typeof args[1] !== 'number'
+  ) {
+    return 'ERROR'
+  } else {
+    for (let i = args[0]; i <= args[1]; i++) {
+      fullArr.push(i)
+    }
 
-};
+    sum = fullArr.reduce(reducer)
+    return sum
+  }
+}
 
 // Do not edit below this line
-module.exports = sumAll;
+module.exports = sumAll
