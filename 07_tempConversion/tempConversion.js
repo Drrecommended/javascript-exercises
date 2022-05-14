0f943f01e5ed7536bc92a4ec77d86344367ee0f1
Tyler French <tylerfrench89@gmail.com>
2022-05-14 16:58:32 -0700

    finished temp conversino excersise

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,11 @@
-const ftoc = function() {
-
+const ftoc = function(fTemp) {
+  const cTemp = (fTemp - 32) * 5/9
+  return Math.round(cTemp * 10) / 10
 };
 
-const ctof = function() {
-
+const ctof = function(cTemp) {
+  const fTemp = cTemp * 9/5 + 32
+  return Math.round(fTemp * 10) / 10
 };
 
 // Do not edit below this line
@@ -11,3 +13,13 @@ module.exports = {
   ftoc,
   ctof
 };
+
+//But Also this way
+   
+// const ftoc = function(f) {
+//   return Math.round((f - 32) * (5/9) * 10) / 10;
+//  };
+ 
+//  const ctof = function(c) {
+//    return Math.round(((c * 9/5) + 32) * 10) / 10;
+//  };
